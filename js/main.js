6517c64d2c47fcb6380d0283c68faf3ed51842c6
natebc72 <nbchristiansen72@gmail.com>
2022-09-23 14:53:27 -0700

    updates

--- js/main.js
+++ js/main.js
@@ -37,3 +37,39 @@ for (var i = 0; i < links.length; i++) {
     ol.appendChild(li);
  };
 
+ //this is my array which I will update every week with a label and url
+const g_work = [
+    {
+        label: "Week 1 Group",
+        url: "z_group_work/week1/week1.html"
+    },
+    {
+        label: "Week 2 Group",
+        url: "z_group_work/week2/week2.html"
+    },
+    {
+        label: "Week 3 Group",
+        url: "z_group_work/week3/week3.html"
+    },
+
+];
+
+//this gets the assignments id from the html
+var ol = document.getElementById("groups");
+
+//this will create a loop to create the li and a elements
+for (var i = 0; i < links.length; i++) {
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    
+    //these reference the array above for info
+    a.href = links[i].url;
+    a.innerHTML = links[i].label;
+    a.target="_blank";
+    
+    //these will append and place the a inside the li and the li inside the ol
+    li.appendChild(a);
+    ol.appendChild(li);
+ };
+
+
