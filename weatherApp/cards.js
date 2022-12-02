b6e681b0bba51b14b54b8d9f26f4ed2804e167ee
natebc72 <nbchristiansen72@gmail.com>
2022-12-01 21:43:27 -0800

    updates

--- weatherApp/cards.js
+++ weatherApp/cards.js
@@ -2,15 +2,13 @@ const cards = document.querySelector('.cards');
 
 
 
+//this submits a city to the savedCities list
 let savedCities = JSON.parse(window.localStorage.getItem('city-list')) ?? [];
 for (city in savedCities) {
     console.log(savedCities)
     buildCards(city)
 };
-
-
-
-
+//Save Button triggers event that saves city to local storage and builds a cards for it
 document.querySelector('.save').addEventListener('click',(ev)=> {
     ev.preventDefault();
     let city = {    
@@ -22,9 +20,16 @@ document.querySelector('.save').addEventListener('click',(ev)=> {
     buildCards(city)
 })
 
-function deleteCity() {}
+//Card delete function
+function deleteCity(deleteId) {
+    savedCities.splice(deleteId, 1);
+    localStorage.setItem("city-list", JSON.stringify(savedCities));
+    
+}
 
+id = savedCities.city
 
+//This function builds the individual cards
 async function buildCards() {   
         const response = await Promise.all([fetch(`${url}${input}&units=imperial&appid=${key}`),]);
         const weatherData = await Promise.all(response.map(r => r.json()))
@@ -47,7 +52,9 @@ async function buildCards() {
         h3.textContent = `${sType}`;
 
         card.setAttribute('class','card')
+        card.setAttribute('id', `${id}`)
         btn.setAttribute('class', 'delete')
+        btn.setAttribute('onclick', `deleteCity(${id})`)
 
         card.appendChild(h1);
         card.appendChild(btn);
